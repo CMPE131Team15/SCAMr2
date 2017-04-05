04a1f3d6c10d91c726592b9987f76484f4c5148e
Awcmon <adcarbine@yahoo.com>
2017-04-04 17:50:24 -0700

    Added Student Model

---
+++ SCAMr2/Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace SCAMr2.Models
+{
+	public class Student
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public string Major { get; set; }
+		public string Year { get; set; }
+		public string Email { get; set; }
+		public string Phone { get; set; }
+		public List<string> UncompletedCourses { get; set; }
+		public List<string> CurrentCourses { get; set; }
+		public List<int> Friends { get; set; }
+	}
+
+	public class StudentDBContext : DbContext
+	{
+		public DbSet<Student> Students { get; set; }
+	}
+}
